7117a8f3858afbcb8210385c894926b489a482d3
sai-tanushka-mamidi <saitanushka26@gmail.com>
2026-01-23 18:15:40 +0530

    Delete Q6.py

--- Q6.py
+++
@@ -1,9 +0,0 @@
-#Q6
-import numpy as np
-import pandas as pd
-df_encoded = pd.get_dummies(df)
-vec1 = df_encoded.iloc[0].values
-vec2 = df_encoded.iloc[1].values
-cosine_similarity = np.dot(vec1, vec2) / 
-    (np.linalg.norm(vec1) * np.linalg.norm(vec2))
-print("Cosine Similarity:", cosine_similarity)
